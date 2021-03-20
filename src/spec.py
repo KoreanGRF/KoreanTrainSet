194e5603870d3a8ba7c6bf5cda5692954788f198
TELK <telk5093@gmail.com>
2021-03-21 02:51:22 +0900

    Add Flat car

--- src/spec.py
+++ src/spec.py
@@ -62,12 +62,13 @@ trainList['METRO_wagon']        = 100,     100,    15,          100,     30,    
 
 trainList['GIMPO_GOLDLINE']     = 80,       80,    10,          100,     30,           500,    (2019, 1, 1)
 trainList['BUSANGIMHAE']        = 70,       70,    10,          100,     30,           500,    (2011, 1, 1)
-trainList['INCHEON_METRO_2']    = 80,       80,    10,          100,     30,           500,    (2016, 1, 1)
-trainList['UISINSEOL']          = 80,       80,    10,           80,     30,           520,    (2017, 1, 1)
-trainList['YONGIN_EVERLINE']    = 80,       80,    10,           80,     30,           500,    (2017, 1, 1)
-trainList['UIJEONGBU']          = 80,       80,    10,           80,     30,           500,    (2012, 1, 1)
+trainList['INCHEON_METRO_2']    = 80,       80,    10,          00,      30,           500,    (2016, 1, 1)
+trainList['UISINSEOL']          = 80,       80,    10,          80,      30,           520,    (2017, 1, 1)
+trainList['YONGIN_EVERLINE']    = 80,       80,    10,          80,      30,           500,    (2017, 1, 1)
+trainList['UIJEONGBU']          = 80,       80,    10,          80,      30,           500,    (2012, 1, 1)
 
 trainList['PASSWG']             = None,     10,    15,          None,    10,           200,    (1950, 1, 1)
+trainList['FLAT_CAR']           = None,     10,    15,          25,      5,            None,   (1950, 1, 1)
 
 
 # Generates spec.pnml
